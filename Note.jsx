6b155ff0eaa950448b26edc0d0de80e1db81cbd0
Pankajgang070 <64552841+Pankajgang070@users.noreply.github.com>
2021-06-27 23:02:22 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Note() {
+  return <div>
+    <div className="note">
+      <h1>Javascript and React.js</h1>
+      <p>This is an amazing bootcamp taken up by Pankaj Kumar
+        on front end webdev including Javascript and react.js
+      </p>
+    </div>
+
+    </div>;
+}
+
+export default Note;
